381f39ebbfc90a3c806da628e1d7564938813462
JieLiao <1281337643@qq.com>
2024-08-23 15:14:05 +0800

    feat: 新增滑动刻度尺组件

--- README.md
+++ README.md
@@ -6,6 +6,17 @@
 
 ### 使用说明
 
+#### SlideRuler
+
+适用于 H5 的滑动刻度尺组件。
+
+目录：components/SlideRuler
+
+使用方法：
+1、复制该组件到项目
+
+```
+
 #### emoji 表情包
 
 emoji 表情包大全
@@ -23,8 +34,10 @@ emoji 表情包大全
 2、引入该组件，具体参数参照 vant Field 组件
 
 ```
+
 import Field from "@/components/Field/index.vue";
 <field v-model="testDate"> </field>
+
 ```
 
 #### DatetimePicker
@@ -38,8 +51,10 @@ import Field from "@/components/Field/index.vue";
 2、引入该组件，具体参数参照 vant3 DatetimePicker 组件
 
 ```
+
 import DatetimePicker from "@/components/DatetimePicker/index.vue";
 <datetime-picker v-model="testDate"> </datetime-picker>
+
 ```
 
 #### chartsExample 目录
@@ -55,20 +70,25 @@ echarts 复杂图表使用示例
 Vue 导出页面为 PDF
 
 ```
+
 <!-- 安装 html2Canvas、JsPDF -->
+
 npm install html2Canvas JsPDF --save
 
 <!-- main.js -->
+
 import htmlToPdf from '@/utils/htmlToPdf'
 Vue.use(htmlToPdf)
 
 <!-- 页面 -->
-<button @click="getPdf(config)">导出PDF</button>
+
+<button @click="getPdf(config)">导出 PDF</button>
 config={
-  id: 导出页面dom id，默认pdf-dom
-  title: 导出PDF文件名称，默认当前时间戳
+id: 导出页面 dom id，默认 pdf-dom
+title: 导出 PDF 文件名称，默认当前时间戳
 }
-config可不传，不传为默认值
+config 可不传，不传为默认值
+
 ```
 
 #### gradientColor.js
@@ -76,8 +96,10 @@ config可不传，不传为默认值
 计算颜色渐变
 
 ```
-直接调用gradientColor方法，传入步长、渐变开始颜色、渐变结束颜色，返回渐变颜色数组
+
+直接调用 gradientColor 方法，传入步长、渐变开始颜色、渐变结束颜色，返回渐变颜色数组
 gradientColor(20,[138,208,136],[241,39,39])
+
 ```
 
 #### 参与贡献
@@ -86,3 +108,4 @@ gradientColor(20,[138,208,136],[241,39,39])
 2.  新建 Feat_xxx 分支
 3.  提交代码
 4.  新建 Pull Request
+```
---
+++ components/SlideRuler/index.vue
@@ -0,0 +1,379 @@
+<!--
+ * @Author: JieLiao
+ * @Date: 2024-08-22 17:14:42
+ * @Description: 滑动刻度尺组件
+-->
+<script setup>
+import { onMounted, reactive } from "vue";
+
+const props = defineProps({
+  // 尺子宽度
+  canvasWidth: {
+    type: Number,
+    default: (document.body.clientWidth || 375) - 24
+  },
+  // 尺子高度
+  canvasHeight: {
+    type: Number,
+    default: 60
+  },
+  // 当前值刻度线宽度
+  currentLineWidth: {
+    type: String,
+    default: "before:w-[2px]"
+  },
+  // 当前值刻度线高度
+  currentLineHeight: {
+    type: String,
+    default: "before:h-[16px]"
+  },
+  // 当前值刻度线颜色
+  currentLineColor: {
+    type: String,
+    default: "before:bg-[--van-blue]"
+  },
+  // 长刻度线高度
+  heightDecimal: {
+    type: Number,
+    default: 16
+  },
+  // 短刻度线高度
+  heightDigit: {
+    type: Number,
+    default: 8
+  },
+  // 长刻度线颜色
+  colorDecimal: {
+    type: String,
+    default: "#B3BBB3"
+  },
+  // 短刻度线颜色
+  colorDigit: {
+    type: String,
+    default: "#B3BBB3"
+  },
+  // 刻度线宽度
+  lineWidth: {
+    type: Number,
+    default: 2
+  },
+  // 两个刻度线之间的像素宽度
+  divide: {
+    type: Number,
+    default: 10
+  },
+  // 刻度字体大小
+  fontSize: {
+    type: Number,
+    default: 16
+  },
+  // 刻度字体颜色
+  fontColor: {
+    type: String,
+    default: "#A7AAB4"
+  },
+  // 刻度字体与上边界距离
+  fontMarginTop: {
+    type: Number,
+    default: 22
+  },
+  // 是否显示状态文本
+  showStatusText: {
+    type: Boolean,
+    default: true
+  },
+  // 尺子最小值
+  minValue: {
+    type: Number,
+    default: 0
+  },
+  // 尺子最大值
+  maxValue: {
+    type: Number,
+    default: 200
+  },
+  // 警告最小值
+  warnMinValue: {
+    type: Number,
+    default: 0
+  },
+  // 警告最大值
+  warnMaxValue: {
+    type: Number,
+    default: 0
+  },
+  // 刻度小数位数
+  precision: {
+    type: Number,
+    default: 1
+  }
+});
+// 尺子当前值
+const currentValue = defineModel("currentValue", {
+  type: Number,
+  default: 100
+});
+const emit = defineEmits(["change"]);
+let canvas;
+const localState = reactive({
+  startX: 0,
+  startY: 0,
+  isTouchEnd: true,
+  touchPoints: []
+});
+const initCanvas = () => {
+  canvas = document.querySelector("#canvasDom");
+  console.log(currentValue.value);
+  handleDreawCanvas();
+};
+const handleDreawCanvas = () => {
+  const context = canvas.getContext("2d");
+  canvas.height = canvas.height;
+  let {
+    canvasWidth,
+    canvasHeight,
+    maxValue,
+    minValue,
+    precision,
+    divide,
+    heightDecimal,
+    heightDigit,
+    lineWidth,
+    colorDecimal,
+    colorDigit,
+    fontSize,
+    fontColor,
+    fontMarginTop
+  } = props;
+  // 计算当前值
+  currentValue.value =
+    currentValue.value > minValue
+      ? currentValue.value < maxValue
+        ? currentValue.value
+        : maxValue
+      : minValue;
+  currentValue.value =
+    (Math.round((currentValue.value * 10) / precision) * precision) / 10;
+  emit("change", currentValue.value);
+  let diffCurrentMin = ((currentValue.value - minValue) * divide) / precision,
+    startValue =
+      currentValue.value - Math.floor(canvasWidth / 2 / divide) * precision;
+  startValue =
+    startValue > minValue
+      ? startValue < maxValue
+        ? startValue
+        : maxValue
+      : minValue;
+  let endValue = startValue + (canvasWidth / divide) * precision;
+  endValue = endValue < maxValue ? endValue : maxValue;
+  // 定义原点
+  let origin = {
+    x:
+      diffCurrentMin > canvasWidth / 2
+        ? (canvasWidth / 2 -
+            ((currentValue.value - startValue) * divide) / precision) *
+          2
+        : (canvasWidth / 2 - diffCurrentMin) * 2,
+    y: canvasHeight * 2
+  };
+  // 定义刻度线样式
+  heightDecimal = heightDecimal * 2;
+  heightDigit = heightDigit * 2;
+  lineWidth = lineWidth * 2;
+  // 定义刻度字体样式
+  fontSize = fontSize * 2;
+  fontMarginTop = fontMarginTop * 2;
+  // 每个刻度所占位的px
+  divide = divide * 2;
+  // 定义每个刻度的精度
+  const derivative = 1 / precision;
+  for (
+    let i = Math.round((startValue / precision) * 10) / 10;
+    i <= endValue / precision;
+    i++
+  ) {
+    context.beginPath();
+    // 画刻度线
+    context.moveTo(origin.x + (i - startValue / precision) * divide, 0);
+    // 画线到刻度高度，10的位数就加高
+    context.lineTo(
+      origin.x + (i - startValue / precision) * divide,
+      i % 10 === 0 ? heightDecimal : heightDigit
+    );
+    context.lineWidth = lineWidth;
+    // 10的位数就加深
+    context.strokeStyle = i % 10 === 0 ? colorDecimal : colorDigit;
+    context.stroke();
+    // 描绘刻度值
+    context.fillStyle = fontColor;
+    context.textAlign = "center";
+    context.textBaseline = "top";
+    if (i % 10 === 0) {
+      context.font = `${fontSize}px Arial`;
+      context.fillText(
+        Math.round(i / 10) / (derivative / 10),
+        origin.x + (i - startValue / precision) * divide,
+        fontMarginTop
+      );
+    }
+    context.closePath();
+  }
+};
+const handleTouchStart = e => {
+  e.preventDefault();
+  if (e || localState.isTouchEnd) {
+    handleTouchPoints(e);
+    const touch = (e.touches && e.touches[0]) || e;
+    localState.startX = touch.pageX;
+    localState.startY = touch.pageY;
+    localState.startT = new Date().getTime(); // 记录手指按下的开始时间
+    localState.isTouchEnd = false; // 当前开始滑动
+  }
+};
+const handleTouchMove = e => {
+  handleTouchPoints(e);
+  const touch = (e.touches && e.touches[0]) || e;
+  const deltaX = touch.pageX - localState.startX;
+  const deltaY = touch.pageY - localState.startY;
+  // 如果X方向上的位移大于Y方向，则认为是左右滑动
+  if (
+    Math.abs(deltaX) > Math.abs(deltaY) &&
+    Math.abs(Math.round(deltaX / props.divide)) > 0
+  ) {
+    if (!handleRebound(deltaX)) return;
+    handleMoveDreaw(deltaX);
+    localState.startX = touch.pageX;
+    localState.startY = touch.pageY;
+  }
+};
+const handleTouchEnd = () => {
+  handleMoveDreaw(handleInertialShift());
+  localState.isTouchEnd = true;
+  localState.touchPoints = [];
+  canvas.style.transform = "translate3d(0, 0, 0)";
+};
+const handleTouchPoints = e => {
+  const touch = (e.touches && e.touches[0]) || e;
+  const time = new Date().getTime();
+  const shift = touch.pageX;
+  localState.touchPoints.push({ time: time, shift: shift });
+};
+const handleInertialShift = () => {
+  let s = 0;
+  if (localState.touchPoints.length >= 4) {
+    const _points = localState.touchPoints.slice(-4),
+      v =
+        ((_points[3].shift - _points[0].shift) /
+          (_points[3].time - _points[0].time)) *
+        1000; // v 手指离开屏幕后的速度px/s
+    const a = 6000; // a 手指离开屏幕后的加速度
+    s = (Math.sign(v) * Math.pow(v, 2)) / (2 * a); // s 手指离开屏幕后惯性距离
+  }
+  return s;
+};
+const handleRebound = deltaX => {
+  const { maxValue, minValue } = props;
+  if (
+    (currentValue.value === minValue && deltaX > 0) ||
+    (currentValue.value === maxValue && deltaX < 0)
+  ) {
+    const reboundX =
+      Math.sign(deltaX) * 1.5988 * Math.pow(Math.abs(deltaX), 0.7962);
+    canvas.style.transform = `translate3d(${reboundX}px, 0, 0)`;
+    return false;
+  }
+  return true;
+};
+const handleMoveDreaw = shift => {
+  const { divide, precision } = props;
+  let moveValue = Math.round(-shift / divide),
+    _moveValue = Math.abs(moveValue);
+  const draw = () => {
+    if (_moveValue < 1) return;
+    currentValue.value += Math.sign(moveValue) * precision;
+    handleDreawCanvas();
+    window.requestAnimationFrame(draw);
+    _moveValue--;
+  };
+  draw();
+};
+onMounted(initCanvas);
+</script>
+
+<template>
+  <div>
+    <div class="mb-[16px] flex">
+      <div
+        class="h-[50px] mx-auto px-[25px] text-[14px] leading-[50px] text-[#fff] rounded-[25px]"
+        :class="[
+          (warnMinValue && currentValue < warnMinValue) ||
+          (warnMaxValue && currentValue > warnMaxValue)
+            ? 'bg-[#EA241E]'
+            : 'bg-[--van-blue]'
+        ]"
+      >
+        <span class="mr-[4px] text-[32px] font-bold">{{ currentValue }}</span>
+        <span v-if="showStatusText">
+          {{
+            (warnMinValue && currentValue < warnMinValue) ||
+            (warnMaxValue && currentValue > warnMaxValue)
+              ? "超标"
+              : "正常"
+          }}
+        </span>
+      </div>
+    </div>
+    <div
+      class="ruler-container m-0 p-0 relative bg-white rounded-[8px]"
+      :class="[
+        currentLineWidth,
+        currentLineHeight,
+        currentLineColor,
+        ((warnMinValue && currentValue < warnMinValue) ||
+          (warnMaxValue && currentValue > warnMaxValue)) &&
+          'warning'
+      ]"
+    >
+      <canvas
+        class="m-[0_auto] p-0 block duration-300"
+        id="canvasDom"
+        :width="canvasWidth * 2"
+        :height="canvasHeight * 2"
+        :style="{ width: `${canvasWidth}px`, height: `${canvasHeight}px` }"
+        @touchstart="handleTouchStart"
+        @touchmove="handleTouchMove"
+        @touchend="handleTouchEnd"
+      ></canvas>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.ruler-container {
+  &::before {
+    position: absolute;
+    left: 50%;
+    top: 0;
+    transform: translateX(-50%);
+    z-index: 1;
+  }
+  &::after {
+    content: "";
+    position: absolute;
+    top: -10px;
+    right: 50%;
+    transform: translateX(50%);
+    border-top: 12px solid var(--van-blue);
+    border-left: 6px solid transparent;
+    border-right: 6px solid transparent;
+  }
+}
+.warning {
+  &::before {
+    background: #ea241e;
+  }
+  &::after {
+    border-top-color: #ea241e;
+  }
+}
+</style>
